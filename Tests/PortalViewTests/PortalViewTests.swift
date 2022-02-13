d7315d65395b43c5150a73b27ca75c5d86468aeb
Muukii <muukii.app@gmail.com>
2022-02-13 23:43:33 +0900

    :evergreen_tree: Update

--- Tests/PortalViewTests/PortalViewTests.swift
+++ Tests/PortalViewTests/PortalViewTests.swift
@@ -2,10 +2,4 @@ import XCTest
 @testable import PortalView
 
 final class PortalViewTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PortalView().text, "Hello, World!")
-    }
 }
